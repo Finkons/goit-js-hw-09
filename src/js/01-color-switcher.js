25019bc6ab6c625c2a992f16e59f25f7d60de428
Andrii Mykhailenko <finkons87@gmail.com>
2022-05-10 22:12:20 +0300

    add timer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+refs = {
+  body: document.querySelector('body'),
+  startButton: document.querySelector('button[data-start]'),
+  stopButton: document.querySelector('button[data-stop]'),
+};
 
+const { body, startButton, stopButton } = refs;
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId = 0;
+stopButton.setAttribute('disabled', 'disablet');
+
+startButton.addEventListener('click', () => {
+  onClickStartButton();
+  startButton.setAttribute('disabled', 'disablet');
+  stopButton.removeAttribute('disabled');
+});
+
+stopButton.addEventListener('click', () => {
+  onClickStopButton();
+  startButton.removeAttribute('disabled');
+  stopButton.setAttribute('disabled', 'disablet');
+});
+
+
+function onClickStartButton() {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000)
+}
+
+function onClickStopButton() {
+  clearInterval(intervalId);
+};
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  inputFormData: document.querySelector('.form'),
+  submitButton: document.querySelector('.form button'),
+};
+let { inputFormData, submitButton } = refs;
+
+submitButton.addEventListener('click', startCreatePromises);
+inputFormData.addEventListener('input', getInputData);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`));
+      } else {
+        reject(Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+      }
+    }, delay);
+  });
 }
+
+let step = 0;
+let delay = 0;
+let amount = 0;
+
+function getInputData(e) {
+  step = Number(e.currentTarget.step.value);
+  delay = Number(e.currentTarget.delay.value);
+  amount = Number(e.currentTarget.amount.value);
+}
+
+function startCreatePromises(e) {
+  e.preventDefault();
+
+
+  for (let i = 1; i <= amount; i += 1) {
+    delay += step;
+
+    createPromise(i, delay)
+      .then(success => console.log('✅ Fulfilled promise'))
+      .catch(error => console.log('❌ Rejected promise'));
+  }
+};
+
